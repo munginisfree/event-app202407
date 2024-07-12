99b099729385950b8295be5c3ad15f2bd7101693
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-12 10:45:57 +0900

    # email verification done

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -3,14 +3,20 @@ import styles from './SignUpForm.module.scss';
 import EmailInput from "./EmailInput";
 import VerificationInput from "./VerificationInput";
 import ProgressBar from "../ProgressBar";
+import PasswordInput from "./PasswordInput";
 const SignUpForm = () => {
     // 현재 몇 단계가 진행되고 있는지
     const [step, setStep] = useState(1);
     //단계가 성공적으로 완료되었는지
     const [success, setSuccess] = useState(false);
+
+    // 입력된 이메일
+    const [eneteredEmail, setEnteredEmail] = useState('');
+
     // 이메일 중복확인이 끝났을 때 호출될 함수
-    const emailSuccessHandler = () => {
+    const emailSuccessHandler = (email) => {
         setSuccess(true);
+        setEnteredEmail(email);
         setTimeout(() => {
             setStep(2);
             setSuccess(false);
@@ -20,9 +26,9 @@ const SignUpForm = () => {
     return (
         <div className={styles.signupForm}>
             <div className={styles.formStepActive}>
-                {/*{step === 1 && <EmailInput onSuccess = {emailSuccessHandler}/>}*/}
-                {/*{step === 2 && <VerificationInput/>}*/}
-                <VerificationInput/>
+                {step === 1 && <EmailInput onSuccess = {emailSuccessHandler}/>}
+                {step === 2 && <VerificationInput email={eneteredEmail}/>}
+                {step === 3 && <PasswordInput/>}
                 {success && <ProgressBar/>}
             </div>
         </div>
