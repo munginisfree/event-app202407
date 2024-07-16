bb62a25463466a0ed6850956083f3b5939b3217d
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-16 09:57:22 +0900

    # login 조건부 랜더링

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LoginForm from "../components/auth/LoginForm";
+import Main from "../components/Main";
 
 const WelcomePage = () => {
-    return <LoginForm/>
+    console.log('Welcome page 실행!!');
+
+    return (
+        <>
+            {<LoginForm/>}
+            {<Main/>}
+        </>
+    );
 };
 
 export default WelcomePage;
